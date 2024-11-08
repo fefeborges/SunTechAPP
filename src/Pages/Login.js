9a06350383ff1fe9ac76ade58e25bc2cc1e7f5d7
fefeborges <fernandavargas1011@gmail.com>
2024-11-08 14:33:48 -0300

    Fer merge com o ck

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -17,12 +17,10 @@ export default function Login() {
     return (
         <ScrollView contentContainerStyle={css.container}>
             <Image source={require("../../assets/suntech.jpeg")} style={css.logo} />
-            <Text style={css.texto}>
-                FAÇA SEU LOGIN  
-            </Text>
+            <Text style={css.texto}>FAÇA SEU LOGIN</Text>
             <TextInput
-                inputMode="E-mail"
-                placeholder="E-mail"
+                inputMode="Email"
+                placeholder="Email"
                 style={css.input}
                 value={email}
                 onChangeText={(digitado) => setEmail(digitado)}
@@ -37,12 +35,11 @@ export default function Login() {
                 onChangeText={(digitado) => setSenha(digitado)}
                 placeholderTextColor="white"
             />
-           
             <TouchableOpacity style={css.btnLogin} onPress={RealizaLogin}>
-                <Text style={css.btnLoginText}>Entrar</Text>
+                <Text style={css.btnLoginText}>ENTRAR</Text>
             </TouchableOpacity>
             <View style={css.forgot}>
-                <Text style={css.forgotText}>Não possui cadastro ainda? Cadastre-se</Text>
+                <Text style={css.forgotText}>Não possui cadastro ainda?</Text><TouchableOpacity><Text style={css.forgotTextBtn}>Cadastre-se</Text></TouchableOpacity>
             </View>
             {error &&
                 <View style={css.error}>
@@ -59,33 +56,43 @@ const css = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         alignContent: "center",
-        backgroundColor: "#263470"
+        backgroundColor: "#263470",
+        color: 'white'
     },
     logo: {
         width: "60%",
         height: 130,
         resizeMode: "cover",
-        top: -155
+        top: -100
     },
     input: {
-        width: "75%",
+        width: "80%",
         height: 50,
         marginBottom: 15,
-        color: "white",
         borderBottomColor: "white",
-        borderBottomWidth: 1
-        
+        borderBottomWidth: 1,
+        fontSize: 17,
+        color: 'white',
     },
     forgot: {
-        width: "90%",
-        marginTop: 10,
-        justifyContent: "center",
-        alignItems: "center",
+        width: "80%",
+        marginTop: 20,
+        alignSelf: 'center',
+        fontSize: 17,
+        display: 'flex',
+        flexDirection: 'row',
     },
     forgotText: {
         color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
+        textAlign: "center",
+        fontSize: 17,
+        marginBottom: 5
+    },
+    forgotTextBtn: {
+        textAlign: "center",
+        fontSize: 17,
+        color: 'white',
+        marginLeft: 5,
     },
     btnLogin: {
         width: "40%",
@@ -94,28 +101,28 @@ const css = StyleSheet.create({
         borderRadius: 10,
         marginTop: 30,
         backgroundColor: "#F8C728",
-        
     },
     btnLoginText: {
         color: "black",
         lineHeight: 45,
         textAlign: "center",
-        fontSize: 15,
-        fontWeight: "bold"
+        fontSize: 18,
     },
     error: {
-        width: "100%",
-        height: 50,
-        marginTop: 30
+        width: "80%",
+        borderRadius: 6,
+        height: 40,
+        marginTop: 30,
+        backgroundColor: 'white',
     },
     errorText: {
-        color: "white",
-        textAlign: "center"
+        textAlign: "center",
+        fontSize: 17,
+        marginTop: 7
     },
     texto: {
         fontSize: 22,
         color: "white",
-        position: "absolute",
-        top: 260
+        marginBottom: 30
     }
 });
